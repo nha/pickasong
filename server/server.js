100afa7c8e10006f4b64574863ab09889f668416
Nico <nha@enssat.fr>
2013-07-03 16:48:16 +0200

    executions de commandes bash

--- server/server.js
+++ server/server.js
@@ -1,5 +1,31 @@
+var sys = Npm.require('sys');
+var exec = Npm.require('child_process').exec;
+
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+
+
+
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
     console.log("Server starting up!");
+
+
+// start VLC with its interface
+exec("ls -la", puts);
+exec("vlc -I", puts);
+
+    console.log("VLC started!");
+	// test call to vlc
+/*
+    Meteor.http.call("POST", "http://api.twitter.com/xyz",
+                 {data: {some: "json", stuff: 1}},
+                 function (error, result) {
+                   if (result.statusCode === 200) {
+                     Session.set("twizzled", true);
+                   }
+                 });
+
+*/
   });
 }
